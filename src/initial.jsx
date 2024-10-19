d9643abbf157238f5aa728934057874c838ebad7
KarthikYadav211 <muntakarthikkumar@gmail.com>
2024-10-19 10:58:57 +0530

    karthik

---
+++ src/initial.jsx
@@ -0,0 +1,18 @@
+function Karthik(){
+    return(
+        <>
+        <div>
+        <h1>Karthik</h1>
+        <img src="https://tse1.mm.bing.net/th?id=OIP.C7T4BbpTK3LSB4cytcLW-AHaE5&pid=Api&P=0&h=180" alt="" width={200} height={200} />
+
+        <img src="https://in.images.search.yahoo.com/images/view;_ylt=AwrPpHByORNnqicwa1y9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzg3NTE4MzAwNmM3MTE2MTk0MjAyMDRjYjMyOWJiZTg2BGdwb3MDMjIEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dimages%2Bof%2Bflowers%26type%3DE211IN885G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D22&w=5184&h=3456&imgurl=images.pexels.com%2Fphotos%2F133464%2Fpexels-photo-133464.jpeg%3Fcs%3Dsrgb%26dl%3Dblack-close-up-color-133464.jpg%26fm%3Djpg&rurl=https%3A%2F%2Fwww.pexels.com%2Fphoto%2Fclose-up-photography-of-purple-petaled-flower-133464%2F&size=625KB&p=images+of+flowers&oid=875183006c711619420204cb329bbe86&fr2=piv-web&fr=mcafee&tt=Close+Up+Photography+of+Purple+Petaled+Flower+%C2%B7+Free+Stock+Photo&b=0&ni=21&no=22&ts=&tab=organic&sigr=ftKgkdXvRrJz&sigb=XciWu78mQQ6m&sigi=8B5zGnbMWwO9&sigt=pspqXFLa2FDC&.crumb=6nzmu80YTXW&fr=mcafee&fr2=piv-web&type=E211IN885G0" alt="" width={200} height={200} />
+        
+        <img src="https://tse1.mm.bing.net/th?id=OIP.qeD10D6gZDreMdeQzyhwOQHaE8&pid=Api&P=0&h=180" alt="" width={200} height={200} />
+        </div>
+
+        </>
+    )
+       
+    
+}
+export default Karthik;
